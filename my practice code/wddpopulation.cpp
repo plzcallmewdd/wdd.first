372872d2cbba22705a0a6a4801a8a2190152be09
plzcallmewdd <88964627+plzcallmewdd@users.noreply.github.com>
2021-11-02 16:52:15 +0800

    Add files via upload

---
+++ my practice code/wddpopulation.cpp
@@ -0,0 +1,20 @@
+// wangdiedang is a good guy! =v=
+#include<iostream>
+#include<vector>
+#include<string>
+#include<limits.h>
+#include<algorithm>
+using namespace std;
+
+class Wddsolution {
+private:
+
+public:
+
+};
+
+int main() {
+	Wddsolution wdd;
+
+	return 0;
+}
